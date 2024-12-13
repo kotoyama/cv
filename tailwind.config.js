107c8760cc006d81a728bd1cc99d8fb8d1063739
kotoyama <snrsrrw@pm.me>
2024-12-13 21:06:53 +0200

    fix tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['src/**/*.{html,njk,md}'],
+  content: ['src/**/*.{html,njk,md}', 'src/**/*.svg'],
   theme: {
     extend: {},
   },
